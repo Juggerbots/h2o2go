5deb5e9f737b3334f51d3956499a3213dc671383
odifmonster <lamanwyner@icloud.com>
2019-09-06 11:50:25 -0400

    changed to getallheaders

--- v1/log_refill.php
+++ v1/log_refill.php
@@ -7,6 +7,7 @@ $response = array();
 $helper = new HelperFunctions();
 $request_params = $_REQUEST;
 $headers = getallheaders();
+echo "why";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($helper->verifyRequiredParams($request_params, array('username', 'amount')) && isset($headers['Authorization'])) {
