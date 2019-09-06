b01c1c33bc26d9132369dd64d8450aa2b45b1422
odifmonster <lamanwyner@icloud.com>
2019-09-06 12:24:13 -0400

    yo fam

--- v1/log_refill.php
+++ v1/log_refill.php
@@ -7,7 +7,6 @@ $response = array();
 $helper = new HelperFunctions();
 $request_params = $_REQUEST;
 $headers = $helper->getallheaders();
-echo "why why why why WHY";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($helper->verifyRequiredParams($request_params, array('username', 'amount')) && isset($headers['Authorization'])) {
