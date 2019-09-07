044de247161de7ae1f6c595f87b74db236c90947
odifmonster <lamanwyner@icloud.com>
2019-09-06 21:49:06 -0400

    some quick changes

--- includes/DbOperation.php
+++ includes/DbOperation.php
@@ -79,12 +79,12 @@ class DbOperation {
         return INVALID_API_KEY;
     }
 
-    public function addLocation($name, $lat, $long, $username, $api_key) {
+    public function addLocation($name, $description, $lat, $long, $username, $api_key) {
         if ($this->isValidApiKey($username, $api_key)) {
             $username = $this->getUser($username);
             $id = $user['id'];
-            $stmt = $this->conn->prepare('insert into refill_locations (name, user_id, latitude, longitude) values (?, ?, ?, ?)');
-            $stmt->bind_param("sidd", $name, $id, $lat, $long);
+            $stmt = $this->conn->prepare('insert into refill_locations (user_id, name, description, latitude, longitude) values (?, ?, ?, ?, ?)');
+            $stmt->bind_param("issdd", $id, $name, $description, $lat, $long);
             $stmt->execute();
             $stmt->close();
             return LOCATION_ADDED;
--- v1/add_location.php
+++ v1/add_location.php
@@ -8,9 +8,10 @@ $helper = new HelperFunctions();
 $request_params = $_REQUEST;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $error_fields = $helper->verifyRequiredParams($request_params, array('name', 'lat', 'long', 'username', 'api_key'));
+    $error_fields = $helper->verifyRequiredParams($request_params, array('name', 'description', 'lat', 'long', 'username', 'api_key'));
     if (!$error_fields['error']) {
         $name = $_POST['name'];
+        $description = $_POST['description'];
         $lat = $_POST['lat'];
         $long = $_POST['long'];
         $username = $_POST['username'];
@@ -18,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         $db = new DbOperation();
 
-        $result = $db->getRefills($name, $lat, $long, $username, $api_key);
+        $result = $db->addLocation($name, $description, $lat, $long, $username, $api_key);
 
         if ($result ==  INVALID_API_KEY) {
             $response['error'] = true;
