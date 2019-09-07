db3c796d25e1cf4565eb57ef9bd517c44685ed56
odifmonster <lamanwyner@icloud.com>
2019-09-06 21:50:39 -0400

    stop. debug time.

--- v1/add_location.php
+++ v1/add_location.php
@@ -17,11 +17,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $username = $_POST['username'];
         $api_key = $_POST['api_key'];
 
+        echo "woot";
+
         $db = new DbOperation();
 
         $result = $db->addLocation($name, $description, $lat, $long, $username, $api_key);
 
-        if ($result ==  INVALID_API_KEY) {
+        if ($result == INVALID_API_KEY) {
             $response['error'] = true;
             $response['message'] = 'Invalid API key.';
         } else {
