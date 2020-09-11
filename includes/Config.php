10b598a3471d19bb84846f57660e754f6214259c
Lâm An Wyner <lamanwyner@Lams-MacBook-Pro.local>
2020-09-11 11:52:17 -0400

    Added wordwrap to vscode settings

--- includes/Config.php
+++ includes/Config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'juggerbots');
+define('DB_PASSWORD', 'temptemp');
 define('DB_HOST', 'h2o2go-database-1.c5wmyocjmi7a.us-east-2.rds.amazonaws.com');
 define('DB_NAME', 'h2o2go');
 define('DB_PORT', 3306);
